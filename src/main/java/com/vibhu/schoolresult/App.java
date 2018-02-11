cb16ae6eb471b80387fd63351575e0f74971fc21
Vibhu Ranjan <vibhu.ranjan@accenture.com>
2018-02-11 11:51:29 +0100

    new project

---
+++ src/main/java/com/vibhu/schoolresult/App.java
@@ -0,0 +1,13 @@
+package com.vibhu.schoolresult;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
